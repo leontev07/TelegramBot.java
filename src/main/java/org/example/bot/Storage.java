ac1219438eb7fae496096c964e19d246600a15ee
leontev07 <leontev07@mail.ru>
2023-06-29 16:46:02 +0300

    Готовый бот

--- src/main/java/org/example/bot/Storage.java
+++ src/main/java/org/example/bot/Storage.java
@@ -7,17 +7,47 @@ public class Storage {
     Storage()
     {
         quoteList = new ArrayList<>();
-        quoteList.add("Весь наш мир скатился в ад,\n" +
-                "Это Ларин виноват \n\nТема Косов.");
-        quoteList.add("Je ungeheuerlicher die Lüge ist, desto eher glaubt die Menge an sie.\n\nAdolf Hitler");
-        quoteList.add("Идея наказать страну, которая обладает крупнейшим ядерным потенциалом, абсурдна сама по себе. И потенциально создаеёт угрозу существованию человечества.\n\nДмитрий Медведев");
+        quoteList.add(
+                "Если у вас заблокировалась учётка, её можно восстановить самостоятельно с помощью ADSelf Service Plus.\n"+
+                "Для регистрации в ADSELF SERVICE PLUS необходимо воспользоваться инструкцией https://drive.google.com/file/d/1dbdgSBYT4GhRNzj3N-EwDWJmT9QdfMLY/view?usp=sharing\n"+
+                "Инструкция по смене пароля - https://drive.google.com/file/d/1TTK_bxI9FO_VsKiptGl1VdU0uRrksgi4/view\n"+
+                "Инструкция по разблокировке учётной записи - https://drive.google.com/file/d/1lrALI0ahCNs0sBqep-Wuw4UW_2UpWDbd/view");
+        quoteList.add("Список ссылок для новичков - https://conf.goods.ru/pages/viewpage.action?pageId=259590143\n"+" \n"+
+                "Инструкции по макросам Content_Helper - https://conf.goods.ru/pages/viewpage.action?pageId=217455969\n"+" \n"+
+                "Инструкция по работе с тикетами https://conf.goods.ru/pages/viewpage.action?pageId=212313495\n"+" \n" +
+                "Список формул Excel и их значения + лайфхаки https://conf.goods.ru/pages/viewpage.action?pageId=217448932");
+        quoteList.add("Контакты контента - https://conf.goods.ru/pages/viewpage.action?pageId=264256730");
+        quoteList.add("Напиши боту @smm_content_support_bot");
+        quoteList.add("Список формул Excel и их значения + лайфхаки https://conf.goods.ru/pages/viewpage.action?pageId=217448932");
+        quoteList.add("Примечания в PIM - Новый формат (Нидерландские, Немецкие, Испанские: ведение данных https://conf.goods.ru/pages/viewpage.action?pageId=242028936");
     }
 
-    String getRandQuote()
+    String getInstruction()
     {
         //получаем случайное значение в интервале от 0 до самого большого индекса
-        int randValue = (int)(Math.random() * quoteList.size());
+        int randValue = 1;
         //Из коллекции получаем цитату со случайным индексом и возвращаем ее
         return quoteList.get(randValue);
     }
+    String getLink()
+    {
+        int value = 0;
+        return quoteList.get(value);
+    }
+    String getContact()
+    {
+        int value = 2;
+        return quoteList.get(value);
+    }
+    String getBot() {
+        int value = 3;
+        return quoteList.get(value);
+    }
+    String getFormula() {
+        int value = 4;
+        return quoteList.get(value);
+    }
+    String getNotes() {
+        int value = 5;
+        return quoteList.get(value);}
 }
--- src/main/java/org/example/bot/TelegramBot.java
+++ src/main/java/org/example/bot/TelegramBot.java
@@ -1,17 +1,30 @@
 package org.example.bot;
 
+import lombok.NonNull;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TelegramBot extends TelegramLongPollingBot {
     //создаем две константы, присваиваем им значения токена и имя бота соответсвтенно
     //вместо звездочек подставляйте свои данные
-    final private String BOT_TOKEN = "6125241522:AAEMa1yp_FrVduHHUzKdnktANvpv3JRrj6U";
-    final private String BOT_NAME = "pimnotification_bot";
+    final private String BOT_TOKEN = "5821570586:AAFGOBCKfM___b5NRhNCZr4cX0C_CCP6Grs";
+    final private String BOT_NAME = "contentinstructions_bot";
+    String userName;
     Storage storage;
+
 
     public TelegramBot()
     {
@@ -30,22 +43,33 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        try{
-            if(update.hasMessage() && update.getMessage().hasText())
-            {
+
+
+        try {
+            if (update.hasMessage() && update.getMessage().hasText()) {
+
                 //Извлекаем из объекта сообщение пользователя
                 Message inMess = update.getMessage();
                 //Достаем из inMess id чата пользователя
                 String chatId = inMess.getChatId().toString();
+
+                //получаем username пользователя
+                String username = update.getMessage().getFrom().getUserName();
+                userName = username;
+                //логируем в консоли какой пользователь юзал бота
+                System.out.println(username);
                 //Получаем текст сообщения пользователя, отправляем в написанный нами обработчик
                 String response = parseMessage(inMess.getText());
                 //Создаем объект класса SendMessage - наш будущий ответ пользователю
                 SendMessage outMess = new SendMessage();
 
+
+
                 //Добавляем в наше сообщение id чата а также наш ответ
+
                 outMess.setChatId(chatId);
                 outMess.setText(response);
-
+                setButtons(outMess);
                 //Отправка в чат
                 execute(outMess);
             }
@@ -53,22 +77,82 @@ public class TelegramBot extends TelegramLongPollingBot {
             e.printStackTrace();
         }
     }
+
     public String parseMessage(String textMsg) {
         String response;
 
+
+
+
+
         //Сравниваем текст пользователя с нашими командами, на основе этого формируем ответ
-        if(textMsg.equals("/start"))
-            response = "Приветствую, бот знает много цитат. Жми /get, чтобы получить случайную из них";
-        else if(textMsg.equals("/start") || textMsg.equals("Привет"))
-            response = storage.getRandQuote();
-        else if(textMsg.equals("/get"))
-            response = storage.getRandQuote();
-        else if(textMsg.equals("/get") || textMsg.equals("Расскажи"))
-            response = storage.getRandQuote();
+        if(textMsg.equals("/start") || (textMsg.toLowerCase()).equals("старт") || (textMsg.toLowerCase()).equals("привет"))
+            response = "Приветствую, "+userName+"\nЖми /instruction или Инструкции, чтобы получить ссылки на инструкции.\n" +
+                    "Жми /account_blocking или Разблокировка для получения руководства при блокировке аккаунта. \n" +
+                    "Жми /contacts или Контакты контента, чтобы посмотреть контакты сотрудников\n" +
+                    "Жми /needhelp или Позови человека, далее перейди по ссылке и опиши проблему\n" +
+                    "Жми /formulas или Формулы Excel, чтобы посмотреть полезнные формулы и лайфхаки\n" +
+                    "Жми /notes или Примечания (новый формат), чтобы открыть инструкцию по примечаниям";
+
+        //else if(textMsg.equals("/start") || textMsg.equals("Привет"))
+           // response = storage.getRandQuote();
+        else if(textMsg.equals("/instruction") || (textMsg.toLowerCase()).equals("инструкции")) {
+            response = storage.getInstruction();
+        }else if(textMsg.equals("/account_blocking") || (textMsg.toLowerCase()).equals("разблокировка"))
+            response = storage.getLink();
+        else if(textMsg.equals("/contacts") || (textMsg.toLowerCase()).equals("контакты контента"))
+            response = storage.getContact();
+        else if(textMsg.equals("/needhelp") || (textMsg.toLowerCase()).equals("позови человека"))
+            response = storage.getBot();
+        else if(textMsg.equals("/formulas") || (textMsg.toLowerCase()).equals("формулы excel"))
+            response = storage.getFormula();
+        else if(textMsg.equals("/notes") || (textMsg.toLowerCase()).equals("примечания (новый формат)"))
+            response = storage.getNotes();
         else
             response = "Сообщение не распознано";
 
+
         return response;
+
     }
+    public synchronized void setButtons(SendMessage sendMessage) {
+        // Создаем клавиуатуру
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+
+
+        // Создаем список строк клавиатуры
+        List<KeyboardRow> keyboard = new ArrayList<>();
+
+        // Первая строчка клавиатуры
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+        // Добавляем кнопки в первую строчку клавиатуры
+        keyboardFirstRow.add(new KeyboardButton("Инструкции"));
+        keyboardFirstRow.add(new KeyboardButton("Позови человека"));
+        keyboardFirstRow.add(new KeyboardButton("Примечания (новый формат)"));
+
+        // Вторая строчка клавиатуры
+        KeyboardRow keyboardSecondRow = new KeyboardRow();
+        // Добавляем кнопки во вторую строчку клавиатуры
+        keyboardSecondRow.add(new KeyboardButton("Разблокировка"));
+        keyboardSecondRow.add(new KeyboardButton("Формулы Excel"));
+        // Первая строчка клавиатуры
+        KeyboardRow keyboardThirdRow = new KeyboardRow();
+        // Добавляем кнопки в первую строчку клавиатуры
+        keyboardThirdRow.add(new KeyboardButton("Контакты контента"));
+
+        // Добавляем все строчки клавиатуры в список
+        keyboard.add(keyboardFirstRow);
+        keyboard.add(keyboardSecondRow);
+        keyboard.add(keyboardThirdRow);
+        // и устанваливаем этот список нашей клавиатуре
+        replyKeyboardMarkup.setKeyboard(keyboard);
+    }
+
+
 }
 
